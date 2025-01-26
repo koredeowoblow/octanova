714d9294555e08f55ab1390fe5ba6eada68da93e
koredeowoblow <100287793+koredeowoblow@users.noreply.github.com>
2025-01-26 21:28:37 +0100

    structure

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
